08125b7b786f7fc7bc53c16ae8ef7d8d3693b978
caioian <iancaio015@gmail.com>
2025-06-10 17:59:31 -0300

    Fix build lint

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,9 +1,14 @@
 import { ProductPage } from '@/components/pages/products';
 import { BaseLayout } from '@/layout/BaseLayout';
 
-export default async function ProductPageRoute({ params }: { params: { id: string } }) {
-  const resolvedParams = await Promise.resolve(params);
-  const productId = parseInt(resolvedParams.id, 10);
+type ProductPageRouteProps = {
+  params: Promise<{ id: string }>;
+};
+
+
+export default async function ProductPageRoute({ params }: ProductPageRouteProps) {
+  const { id } = await params;
+  const productId = parseInt(id, 10);
 
   return (
     <BaseLayout>
