98b234615c994481fe098c621445fb0796538afe
caioian <iancaio015@gmail.com>
2025-09-26 15:25:51 -0300

    chore(catalog): remove sold-out t-shirt from catalog

--- src/data/products.ts
+++ src/data/products.ts
@@ -48,17 +48,6 @@ export const products: Product[] = [
   },
   {
     id: 4,
-    name: 'Tripside X Carpa Tee Peixe Black',
-    promotionalPrice: 49.99,
-    colorsAvailable: ['Preto'],
-    sizes: ['G'],
-    category: 'camisa',
-    imagesUrlColumn: ['/products/mockups_still/camisasG/TRIPSIDE_X_CARPA_TEE_PEIXE_BLACK_FRONT.png'],
-    imageUrl: '/products/mockups_still/camisasG/TRIPSIDE_X_CARPA_TEE_PEIXE_BLACK_BACK.png',
-    imageAlt: 'Camisa Tripside X Carpa Tee Peixe preta'
-  },
-  {
-    id: 5,
     name: 'NikeSB Tee Basic Black',
     promotionalPrice: 49.99,
     colorsAvailable: ['Preto'],
@@ -69,7 +58,7 @@ export const products: Product[] = [
     imageAlt: 'Camisa NikeSB Tee Basic Black preta'
   },
   {
-    id: 6,
+    id: 5,
     name: 'Nike Tee Ice Code Black',
     promotionalPrice: 49.99,
     colorsAvailable: ['Preto'],
@@ -80,7 +69,7 @@ export const products: Product[] = [
     imageAlt: 'Camisa Nike Tee Ice Code Black preta'
   },
   {
-    id: 7,
+    id: 6,
     name: 'High Tee Stamp Black',
     promotionalPrice: 49.99,
     colorsAvailable: ['Preto'],
@@ -91,7 +80,7 @@ export const products: Product[] = [
     imageAlt: 'Camisa High Tee Stamp Black preta'
   },
   {
-    id: 8,
+    id: 7,
     name: 'Nike Tee Air 90 Black',
     promotionalPrice: 49.99,
     colorsAvailable: ['Preto'],
@@ -102,7 +91,7 @@ export const products: Product[] = [
     imageAlt: 'Camisa Nike Tee Air 90 Black preta'
   },
   {
-    id: 9,
+    id: 8,
     name: 'Short Linho Preto',
     promotionalPrice: 39.99,
     colorsAvailable: ['Preto'],
@@ -113,7 +102,7 @@ export const products: Product[] = [
     imageAlt: 'Short Linho Preto confortável e versátil para o dia a dia'
   },
   {
-    id: 10,
+    id: 9,
     name: 'Short Linho Off',
     promotionalPrice: 39.99,
     colorsAvailable: ['Off'],
